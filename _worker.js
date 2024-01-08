129a63fd7c70e84454e10cbb7328005dbae568fc
JOY <71791037+wuzhuohua168@users.noreply.github.com>
2024-01-08 10:19:42 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,12 @@
+export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        // 修改下方的example.com为自己的节点ip/域名
+        url.hostname="example.com";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      return env.ASSETS.fetch(request);
+    }
+  };
