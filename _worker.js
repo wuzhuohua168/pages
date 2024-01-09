18dc109d426abd13d2768f241952bc3844115ded
JOY <71791037+wuzhuohua168@users.noreply.github.com>
2024-01-09 18:07:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,17 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        // 修改下方的example.com为自己的节点ip/域名
-        url.hostname="172.64.93.79";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      return env.ASSETS.fetch(request);
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      // 你的代理IP，替换成你希望的固定IP
+      let proxyIP = "172.64.93.79";
+      
+      // 使用代理IP
+      let proxyUrl = new URL(request.url);
+      proxyUrl.hostname = proxyIP;
+
+      let new_request = new Request(proxyUrl, request);
+      return fetch(new_request);
     }
-  };
+    return env.ASSETS.fetch(request);
+  }
+};
