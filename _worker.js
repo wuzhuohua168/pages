9e66cf4d3fe75ccc58c9047e9a9e7aaf3422b4de
JOY <71791037+wuzhuohua168@users.noreply.github.com>
2024-01-10 09:22:40 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -1,17 +1,21 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
+
+    if (request.headers.host === 'example.com') {
       // 你的代理IP，替换成你希望的固定IP
       let proxyIP = "172.64.93.79";
-      
+
       // 使用代理IP
       let proxyUrl = new URL(request.url);
       proxyUrl.hostname = proxyIP;
 
       let new_request = new Request(proxyUrl, request);
+      new_request.headers['X-Custom-Header'] = 'value';
+
       return fetch(new_request);
     }
+
     return env.ASSETS.fetch(request);
   }
 };
